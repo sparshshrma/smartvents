09443c2efce6acd266111e5707d62f996fb5814a
sparshshrma <sparsh0508s@gmail.com>
2023-08-02 20:35:24 -0600

    okie'

--- server/server.mjs
+++ server/server.mjs
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://testAdmin:testAdmin@cluster0.xfuh1g6.mongodb.net/?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://testAdmin:testAdmin@cluster0.xfuh1g6.mongodb.net/?retryWrites=true&w=majority');
 
 //generic route for testing if server is live
 app.get('/', (req,res)=>{
